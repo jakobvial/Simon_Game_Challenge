19f57a8ca6de1ff7018c076bfecff625890084f5
Jakob Viktor Albrechtsen <jakobvial@gmail.com>
2022-09-24 14:32:29 +0200

    End step 8, section 15

--- game.js
+++ game.js
@@ -1,28 +1,56 @@
+//region Variables
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+//endregion
+
+//region Events
+let gameStarted = false;
+$(function () {
+    $(document).on("keydown", function (evt) {
+        if (!gameStarted && evt.key === "a") {
+            gameStarted = true;
+            nextSequence();
+        }
+    });
+});
+
+$("." + "btn").on("click", function () {
+    if (!sequenceStarted) {
+        return;
+    }
+
+    let userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+
+    makeSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    let answer = checkAnswer();
+
+    if (!answer) {
+        $("h1").text("You lost!");
+    }
+});
+//endregion
+
+//region Helper functions
+let sequenceStarted = false;
+let level = 0;
 
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
     makeSound(randomChosenColour);
     animateHint(randomChosenColour);
 
+    $("h1").text("Level " + (level + 1));
+    level++;
+
+    sequenceStarted = true;
 }
-
-$(document).ready(function () {
-    $(document).one("keydown", function () {
-        nextSequence();
-    });
-});
-
-$("." + "btn").on("click", function () {
-    let userChosenColour = this.id;
-    userClickedPattern.push(userChosenColour);
-    makeSound(userChosenColour);
-    animatePress(userChosenColour);
-});
 
 function animateHint(colourClicked) {
     $("#" + colourClicked).fadeOut(100).fadeIn(100);
@@ -57,3 +85,31 @@ function makeSound(colourClicked) {
             console.log(colourClicked);
     }
 }
+
+//Check that the pressed button is correct
+function checkAnswer() {
+    let indexLatestClick = userClickedPattern.length - 1;
+    if (userClickedPattern[indexLatestClick].localeCompare(gamePattern[indexLatestClick]) !== 0) {
+        console.log("Wrong");
+        return false;
+    }
+    console.log("Success");
+    if (userClickedPattern.length === gamePattern.length) {
+        resetForNewSequence();
+    }
+    return true;
+}
+
+/*If the complete user sequence is correct:
+* - Disable the user's ability to click buttons
+* - Update the game pattern
+* - Enable the user's ability to click buttons, having reset the user clicked pattern*/
+function resetForNewSequence() {
+    userClickedPattern = [];
+    sequenceStarted = false;
+    setTimeout(function () {
+        nextSequence();
+    }, 1000);
+}
+
+//endregion
